93e95d50ec9f261202e1c4234d536d8f1d70578a
Scott-Fukuda <sdf72@cornell.edu>
2024-09-21 19:02:19 -0400

    Delete wgsi.py

--- wgsi.py
+++
@@ -1,4 +0,0 @@
-from app import app  # Assuming your Flask app instance is named 'app' in app.py
-
-if __name__ == "__main__":
-    app.run()  # Not needed for WSGI, but can be useful for local testing
